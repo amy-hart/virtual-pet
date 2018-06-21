1a4670e01d7e69d588223a4796567720f8a84625
Amy Hart <amy@pageplay.com>
2018-06-21 18:11:12 +0100

    Virtual Pet progress up to Step 8

--- _tests_/virtualpet.test.js
+++ _tests_/virtualpet.test.js
@@ -3,10 +3,75 @@ const {
 } = require('../src/virtualpet.js');
 
 describe('Pet', () => {
-  it('returns a pet with a name', () => {
+  it('Returns a pet with the correct given name', () => {
     const newPet = new Pet('Fido');
-    expect(newPet).toEqual({
-      name: 'Fido',
-    });
+    expect(newPet.name).toEqual('Fido');
+  });
+  it('Returns a pet with the starting age of 0', () => {
+    const newPet = new Pet("Fido");
+    expect(newPet.age).toEqual(0);
+  });
+  it('Returns a pet with the hunger level of 0', () => {
+    const newPet = new Pet("Fido");
+    expect(newPet.hunger).toEqual(0);
+  });
+  it('Returns a pet with the fitness level of 10', () => {
+    const newPet = new Pet("Fido");
+    expect(newPet.fitness).toEqual(10);
+  });
+  it('Increments age by 1', () => {
+    const newPet = new Pet("Fido");
+    newPet.growUp();
+    expect(newPet.age).toEqual(1);
+  });
+  it('Increments hunger by 1', () => {
+    const newPet = new Pet("Fido");
+    newPet.growUp();
+    expect(newPet.hunger).toEqual(5);
+  });
+  it('Decrement fitness by 3', () => {
+    const newPet = new Pet("Fido");
+    newPet.growUp();
+    expect(newPet.fitness).toEqual(7);
+  });
+});
+
+describe('Walk', () => {
+	it('Increments fitness by 4, and caps fitness at 10', () => {
+	    const jim = new Pet("Fido");
+	    jim.fitness = 10;
+	    jim.walk();
+
+	    const bob = new Pet("Fido");
+	    bob.fitness = 7;
+	    bob.walk();
+
+	    const billy = new Pet("Fido");
+	    billy.fitness = 4;
+	    billy.walk();
+
+	    const ben = new Pet("Fido");
+	    ben.fitness = 6;
+	    ben.walk();
+
+    expect(jim.fitness).toEqual(10);
+    expect(bob.fitness).toEqual(10);
+    expect(billy.fitness).toEqual(8);
+    expect(ben.fitness).toEqual(10);
+  });
+});
+
+describe('Feed', () => {
+	it('Decrements hunger by 3, and caps hunger at 0', () => {
+	    const jim = new Pet("Fido");
+	    jim.hunger = 0;
+	    jim.feed();
+
+	    const bob = new Pet("Fido");
+	    bob.hunger = 5;
+	    bob.feed();
+
+    expect(jim.hunger).toEqual(0);
+    expect(bob.hunger).toEqual(2);
   });
 });
--- src/virtualpet.js
+++ src/virtualpet.js
@@ -1,7 +1,63 @@
+const maximumFitness = 10;
+
 function Pet(name){
-	this.name = name
+	this.name = name,
+	this.age = 0,
+	this.hunger = 0,
+	this.fitness = 10
 }
+
+Pet.prototype = {
+	status: function(){
+		console.log("Your pet's name is " + this.name + ".");
+		console.log(this.name + " is " + this.age + " years old.");
+		console.log(this.name + " has a hunger level of " + this.hunger + ".");
+		console.log(this.name + " has a fitness level of " + this.fitness + ".");
+	},
+	growUp: function(){
+		this.age += 1;
+		this.hunger += 5;
+		this.fitness -= 3;
+	},
+	walk: function(){
+		if((this.fitness + 4) >= maximumFitness){
+			this.fitness = 10;
+		}else{
+			this.fitness += 4;
+		}
+	},
+	feed: function(){
+		if((this.hunger - 3) <= 0){
+			this.hunger = 0;
+		}else{
+			this.hunger -= 3;
+		}
+	},
+	checkUp: function(){
+		if(this.fitness <= 3){
+			console.log("I need a walk");	
+		}
+		if(this.hunger >= 5){
+			console.log("I am hungry");	
+		}
+		if((this.fitness <= 3) && (this.hunger >= 5)){
+			console.log("I am hungry AND I need a walk");	
+		}
+		if(!(this.fitness <= 3) && !(this.hunger >= 5)){
+			console.log("I feel great!");	
+		}
+	}
+}
+
+// myPet = new Pet("Bobby");
 
 module.exports = {
 	Pet
 }
+
+
+if the pet's fitness is 3 or less, it should return 'I need a walk'.
+if the pet's hunger is 5 or more, it should return 'I am hungry'.
+if both of the above are true, it should return 'I am hungry AND I need a walk'
+
+if neither of the above are true, it should return 'I feel great!'
